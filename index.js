2d7299ea2a6707198447c7aae0737752f12d4865
Shimul Hossain <shimultechtips@gmail.com>
2024-03-02 17:42:43 +0600

    New Commit

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-console.log("Hello World");
-console.log("Shimul Hossain");
-console.log("Ummul Faria Maliha");
+// Class
+class Person {
+    dob = "05-05-2001";
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let person1 = new Person("shimul", 22);
+console.log(person1);
+
+class Men extends Person {
+    constructor(name, age, sex) {
+        super(name, age);
+        this.sex = sex;
+    }
+}
+
+let personMen = new Men("shimul", 22, "male");
+console.log(personMen);
