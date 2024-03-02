10273dcb3c85868d7b80e60d266478ea0092aa0e
Shimul Hossain <shimultechtips@gmail.com>
2024-03-02 17:45:55 +0600

    New Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Class
 class Person {
-    dob = "05-05-2001";
+    dob = "05-05-2001"; // Object Property Outside Constructor.
     constructor(name, age) {
         this.name = name;
         this.age = age;
@@ -10,9 +10,10 @@ class Person {
 let person1 = new Person("shimul", 22);
 console.log(person1);
 
+// Extended Class
 class Men extends Person {
-    constructor(name, age, sex) {
-        super(name, age);
+    constructor(name, age, sex) { // Parent Class's Parameters + New Parameters
+        super(name, age); // Parent Class's Parameters
         this.sex = sex;
     }
 }
